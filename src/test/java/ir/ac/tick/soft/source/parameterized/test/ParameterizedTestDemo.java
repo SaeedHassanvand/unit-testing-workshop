c70693091c3f8fff70ef615b03b265f72e0c4dcb
s.hasanvand <saeid.hasanvand.s@gmail.com>
2020-11-24 22:41:16 +0330

    Add test class for parameterized test

---
+++ src/test/java/ir/ac/tick/soft/source/parameterized/test/ParameterizedTestDemo.java
@@ -0,0 +1,147 @@
+package ir.ac.tick.soft.source.parameterized.test;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.*;
+
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
+import java.util.Arrays;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+import static org.junit.jupiter.params.provider.EnumSource.Mode.EXCLUDE;
+import static org.junit.jupiter.params.provider.EnumSource.Mode.MATCH_ALL;
+
+public class ParameterizedTestDemo {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"RAZ", "RADAR", "RAZIYANE"})
+    void palindromes(String candidate) {
+        assertTrue(candidate.startsWith("RA"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void testWithValueSource(int argument) {
+        assertTrue(argument > 0 && argument < 4);
+    }
+
+    @ParameterizedTest
+    @NullSource
+    @EmptySource
+    @ValueSource(strings = {" ", "   ", "\t", "\n"})
+    void nullEmptyAndBlankStrings(String text) {
+        assertTrue(text == null || text.trim().isEmpty());
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {" ", "   ", "\t", "\n"})
+    void nullEmptyAndBlankStringsv2(String text) {
+        assertTrue(text == null || text.trim().isEmpty());
+    }
+
+    @ParameterizedTest
+    @EnumSource(ChronoUnit.class)
+    void testWithEnumSource(TemporalUnit unit) {
+        assertNotNull(unit);
+    }
+
+    @ParameterizedTest
+    @EnumSource
+    void testWithEnumSourceWithAutoDetection(ChronoUnit unit) {
+        assertNotNull(unit);
+    }
+
+    @ParameterizedTest
+    @EnumSource(names = {"DAYS", "HOURS"})
+    void testWithEnumSourceInclude(ChronoUnit unit) {
+        assertTrue(EnumSet.of(ChronoUnit.DAYS, ChronoUnit.HOURS).contains(unit));
+    }
+
+    @ParameterizedTest
+    @EnumSource(mode = EXCLUDE, names = {"ERAS", "FOREVER"})
+    void testWithEnumSourceExclude(ChronoUnit unit) {
+        assertFalse(EnumSet.of(ChronoUnit.ERAS, ChronoUnit.FOREVER).contains(unit));
+    }
+
+    @ParameterizedTest
+    @EnumSource(mode = MATCH_ALL, names = "^.*DAYS$")
+    void testWithEnumSourceRegex(ChronoUnit unit) {
+        assertTrue(unit.name().endsWith("DAYS"));
+    }
+
+    @ParameterizedTest
+    @MethodSource("stringProvider")
+    void testWithExplicitLocalMethodSource(String argument) {
+        assertNotNull(argument);
+    }
+
+    static Stream<String> stringProvider() {
+        return Stream.of("apple", "banana");
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void testWithDefaultLocalMethodSource(String argument) {
+        assertNotNull(argument);
+    }
+
+    static Stream<String> testWithDefaultLocalMethodSource() {
+        return Stream.of("apple", "banana");
+    }
+
+    @ParameterizedTest
+    @MethodSource("range")
+    void testWithRangeMethodSource(int argument) {
+        assertNotEquals(9, argument);
+    }
+
+    static IntStream range() {
+        return IntStream.range(0, 20).skip(10);
+    }
+
+    @ParameterizedTest
+    @MethodSource("stringIntAndListProvider")
+    void testWithMultiArgMethodSource(String str, int num, List<String> list) {
+        assertEquals(5, str.length());
+        assertTrue(num >= 1 && num <= 2);
+        assertEquals(2, list.size());
+    }
+
+    static Stream<Arguments> stringIntAndListProvider() {
+        return Stream.of(
+                arguments("apple", 1, Arrays.asList("a", "b")),
+                arguments("lemon", 2, Arrays.asList("x", "y"))
+        );
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "apple,         1",
+            "banana,        2",
+            "'lemon, lime', 0xF1"
+    })
+    void testWithCsvSource(String fruit, int rank) {
+        assertNotNull(fruit);
+        assertNotEquals(0, rank);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/two-column.csv", numLinesToSkip = 1)
+    void testWithCsvFileSourceFromClasspath(String country, int reference) {
+        assertNotNull(country);
+        assertNotEquals(0, reference);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/two-column.csv", numLinesToSkip = 1)
+    void testWithCsvFileSourceFromFile(String country, int reference) {
+        assertNotNull(country);
+        assertNotEquals(0, reference);
+    }
+}
